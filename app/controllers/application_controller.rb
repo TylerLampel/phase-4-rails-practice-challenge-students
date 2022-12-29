b355c481c90ec6910e8043ca0bbbb4c04126ddc2
Tyler Lampel <lampel13@gmail.com>
2022-12-29 17:22:15 -0500

    my solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_not_valid
+
+    private
+
+    def record_not_valid(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found(invalid)
+        render json: { error: "#{invalid.model} not found" }, status: :not_found
+    end
 end
